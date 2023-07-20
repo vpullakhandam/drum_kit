d4836265a347e051323e7d9c5246e7d6cd3adeae
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-07-20 23:34:44 +0530

    added key, removed keyCodes

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-window.addEventListener("keydown",function(event){
- console.log(event);
-})
+window.addEventListener("keydown", function (event) {
+  const audio = document.querySelector(`audio[data-key="${event.key}"]`);
+  if(!audio) return;
+  audio.currentTime=0; // rewind to the start
+  audio.play();
+});
