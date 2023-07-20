edd61a1c8c1c059cca53c57880eb0e245dc26bf9
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-07-21 00:06:12 +0530

    finished the js code

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-window.addEventListener("keydown", function (event) {
+function playSound(event) {
   const audio = document.querySelector(`audio[data-key="${event.key}"]`);
-  if(!audio) return;
-  audio.currentTime=0; // rewind to the start
+  const key = document.querySelector(`.key[data-key="${event.key}"]`);
+  if (!audio) return;
+  audio.currentTime = 0; // rewind to the start
   audio.play();
-});
+  // adding the playing class
+  key.classList.add("playing");
+}
+function removeTransition(event) {
+  if (event.propertyName != "transform") return; // if property is not transform, then return. if it is transform, that means the property belongs to all the keys.
+  this.classList.remove("playing");
+  // removing the playing class
+}
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
